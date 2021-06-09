52a5a2476d13fa8f4cb27cb6d226572efa37b5d4
Ben Brill <benbrill101@gmail.com>
2021-06-08 17:54:42 -0700

    Add new teams

--- sched_extractor.py
+++ sched_extractor.py
@@ -6,8 +6,8 @@ import os
 
 ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
 
-def get_ticket_info():
-    url = "https://api.stubhub.com/sellers/search/events/v3?venueId=744&performerId=1061&parking=false&sort=eventDateLocal%20asc&rows=81"
+def get_ticket_info(teamID, venueID):
+    url = f"https://api.stubhub.com/sellers/search/events/v3?venueId={venueID}&performerId={teamID}&parking=false&sort=eventDateLocal%20asc&rows=81"
     headers = { 
     "Authorization": "Bearer " + ACCESS_TOKEN,
     "Accept": "application/json"
@@ -31,10 +31,16 @@ def create_df(r):
     return df
 
 def main():
-    r = get_ticket_info()
-    df = create_df(r)
-    with sqlite3.connect("tickets.db") as conn: 
-        df.to_sql("extraction", conn, if_exists = "append", index = False)
+    teams = {"Dodgers" : {"teamID": 1061, "venueID": 744},
+         "Angels": {"teamID": 1062, "venueID" : 9763},
+         "Giants" : {"teamID" : 197, "venueID" : 82}}
+    for team in teams:
+        teamID = teams[team]["teamID"]
+        venueID = teams[team]["venueID"]
+        r = get_ticket_info(teamID, venueID)
+        df = create_df(r)
+        with sqlite3.connect("tickets.db") as conn: 
+            df.to_sql("extraction", conn, if_exists = "append", index = False)
 
 if __name__ == "__main__":
     main()
